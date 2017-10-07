4bf1f60f2c4d3de291685df22a6ea2f077a7ef29
Jourdan Lieblich <jslieblich@gmail.com>
2017-10-06 17:00:27 -0700

    Initial Commit
    Have splash screen in place and Sketch file of the end goal UI & functionality

---
+++ src/c/main_window.c
@@ -0,0 +1,63 @@
+#include <pebble.h>
+
+#include "src/c/main_window.h"
+
+static Window *window;
+static TextLayer *text_layer;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Up");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+void setup_main_layer(Window *window) {
+  
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  
+}
+
+static void window_load(Window *window) {
+  setup_main_layer(window);
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+void main_window_create() {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+void main_window_destroy() {
+  window_destroy(window);
+}
+
+Window* main_window_get_window(){
+  return window;
+}
---
+++ src/c/splash_window.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void splash_window_create();
+
+void splash_window_destroy();
+
+Window *splash_window_get_window();
