1dc8878eb6af7d50f5eca123aa5e5f8459dc8efc
Jourdan Lieblich <jslieblich@gmail.com>
2017-10-06 18:24:08 -0700

    Made the Sidebar with "UI"
    Added a few images to the side to show the functionality of the pebble nav buttons and began to think on how to implement the timer proper

--- src/c/main_window.c
+++ src/c/main_window.c
@@ -2,19 +2,31 @@
 
 #include "src/c/main_window.h"
 
+
+// Using a text layer for now to specify the background and sidebar, 
+// I'm thinking there's a more specific way to do that
+
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *background_layer;
+static TextLayer *sidebar_layer;
+static GBitmap *large_zzz;
+static GBitmap *small_zzz;
+static GBitmap *stopwatch;
+static BitmapLayer *large_zzz_layer;
+static BitmapLayer *small_zzz_layer;
+static BitmapLayer *stopwatch_layer;
+
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  text_layer_set_text(background_layer, "Select");
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  text_layer_set_text(background_layer, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  text_layer_set_text(background_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -23,24 +35,61 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-void setup_main_layer(Window *window) {
+void setup_background_layer(Window *window) {
   
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  
+  background_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  text_layer_set_font(background_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
+  text_layer_set_background_color(background_layer, GColorBlack);
+  layer_add_child(window_layer, text_layer_get_layer(background_layer));
+}
 
-  text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+void setup_sidebar_layer(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  // Sidebar Layer setup
+  sidebar_layer = text_layer_create(GRect(100, 0, 44, bounds.size.h));
+  text_layer_set_background_color(sidebar_layer, GColorWhite);
+  layer_add_child(window_layer, text_layer_get_layer(sidebar_layer));
+  
+  // Large ZZZ image setup
+  large_zzz = gbitmap_create_with_resource(RESOURCE_ID_ZZZ_LARGE);
+  large_zzz_layer = bitmap_layer_create(GRect(102, 0, 41, 55));
+  bitmap_layer_set_compositing_mode(large_zzz_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(large_zzz_layer, large_zzz);
+  layer_add_child(window_layer, bitmap_layer_get_layer(large_zzz_layer));
+  
+  // Small ZZZ image setup
+  small_zzz = gbitmap_create_with_resource(RESOURCE_ID_ZZZ_SMALL);
+  small_zzz_layer = bitmap_layer_create(GRect(108, 123, 30, 40));
+  bitmap_layer_set_compositing_mode(small_zzz_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(small_zzz_layer, small_zzz);
+  layer_add_child(window_layer, bitmap_layer_get_layer(small_zzz_layer));
+  
+  // Stopwatch image setup
+  stopwatch = gbitmap_create_with_resource(RESOURCE_ID_STOPWATCH);
+  stopwatch_layer = bitmap_layer_create(GRect(102, 65, 40, 40));
+  bitmap_layer_set_compositing_mode(stopwatch_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(stopwatch_layer, stopwatch);
+  layer_add_child(window_layer, bitmap_layer_get_layer(stopwatch_layer));
   
 }
 
 static void window_load(Window *window) {
-  setup_main_layer(window);
+  setup_background_layer(window);
+  setup_sidebar_layer(window);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(background_layer);
+  text_layer_destroy(sidebar_layer);
+  bitmap_layer_destroy(large_zzz_layer);
+  bitmap_layer_destroy(small_zzz_layer);
+  bitmap_layer_destroy(stopwatch_layer);
 }
 
 void main_window_create() {
---
+++ src/c/timer.c
@@ -0,0 +1,25 @@
+#include <pebble.h>
+#include "timer.h"
+
+// struct Timer{
+  
+//   float time;
+//   int napping = 0;
+  
+//   float getTime(){
+//     return time;
+//   }
+  
+//   void setTime(float newTime){
+//     time = newTime;
+//   }
+  
+//   void napSwitch(){
+//     if(napping){
+//       napping = 0;
+//     } else {
+//       napping = 1;
+//     }
+//   }
+  
+// }
